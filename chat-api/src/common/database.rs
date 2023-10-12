a51000246cea025c3ac04cfb5273b6d9e06c484e
Griffin Martin <me@griffinmartin.dev>
2023-10-12 00:36:21 -0600

    feat: Api: Setup basic handler with routing, controller and db connection

---
+++ chat-api/src/common/database.rs
@@ -0,0 +1,19 @@
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+use std::time::Duration;
+
+pub async fn connect() -> PgPool {
+    // todo use env variables matched to docker-compose
+    let db_connection_str = "postgres://postgres:password@localhost:5432/pidgeon".to_string();
+
+    println!("{}", db_connection_str);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
+
+    return pool;
+}
