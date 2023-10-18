65a1c99a57455d20f7c395bcc2836489a85f7020
Griffin Martin <me@griffinmartin.dev>
2023-10-18 00:24:13 -0600

    build: Dockerize api and db (#9)

--- chat-api/src/common/database.rs
+++ chat-api/src/common/database.rs
@@ -1,18 +1,29 @@
 use sqlx::{postgres::PgPoolOptions, PgPool};
 
+use dotenv::dotenv;
+
 use std::time::Duration;
 
 #[allow(clippy::all)]
 pub async fn connect() -> PgPool {
-    // todo use env variables matched to docker-compose
-    let db_connection_str = "postgres://postgres:password@localhost:5432/pidgeon".to_string();
+    dotenv().ok();
+
+    let db_connection_str = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
 
     println!("{}", db_connection_str);
 
-    return PgPoolOptions::new()
+    let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(3))
         .connect(&db_connection_str)
         .await
-        .expect("can't connect to database");
+        .unwrap_or_else(|_| panic!("Error connecting to {}", db_connection_str));
+
+    // migrations (may move into build process)
+    sqlx::migrate!("./migrations").
+        run(&pool)
+        .await
+        .unwrap_or_else(|_| panic!("Error running migrations"));
+
+    pool
 }
